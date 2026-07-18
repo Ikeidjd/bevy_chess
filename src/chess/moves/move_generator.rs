bbee823f0fd83f794ab282d845682728f3fef2b3
Ikeidjd <ikeidjd2@gmail.com>
2026-07-18 21:45:35 +0200

    Changed pub -> pub (crate) in the places where I missed (where I pressed undo after search and replace)

--- src/chess/moves/move_generator.rs
+++ src/chess/moves/move_generator.rs
@@ -4,7 +4,7 @@ use bevy::{ecs::query::QueryFilter, platform::collections::HashMap, prelude::*};
 
 use crate::chess::{board::Board, markers::{MarkerBoard, PieceMarker}, moves::moves::{GenerateMovesEvent, Moves}, piece::{Piece, PieceColor}, position::Position};
 
-pub trait MoveGenerator {
+pub (crate) trait MoveGenerator {
     fn generate<F: QueryFilter>(&self, commands: &mut Commands, moves: &mut Moves, board: &Board, position: Position, color: PieceColor, piece_colors: Query<&PieceColor, F>,
         allow_moves: bool, allow_captures: bool);
 
@@ -22,18 +22,18 @@ pub trait MoveGenerator {
 }
 
 #[derive(Component)]
-pub struct MoveOnly<MoveGen: MoveGenerator + Component>(pub MoveGen);
+pub (crate) struct MoveOnly<MoveGen: MoveGenerator + Component>(pub (crate) MoveGen);
 
 #[derive(Component)]
-pub struct CaptureOnly<MoveGen: MoveGenerator + Component>(pub MoveGen);
+pub (crate) struct CaptureOnly<MoveGen: MoveGenerator + Component>(pub (crate) MoveGen);
 
 #[derive(Component)]
-pub struct CapturesMarkers<MoveGen: MoveGenerator + Component, Marker: PieceMarker + Component>(pub MoveGen, pub PhantomData<Marker>);
+pub (crate) struct CapturesMarkers<MoveGen: MoveGenerator + Component, Marker: PieceMarker + Component>(pub (crate) MoveGen, pub (crate) PhantomData<Marker>);
 
 #[derive(Component, Default)]
-pub struct CapturesMarkersAlways<Marker: PieceMarker + Component>(pub PhantomData<Marker>);
+pub (crate) struct CapturesMarkersAlways<Marker: PieceMarker + Component>(pub (crate) PhantomData<Marker>);
 
-pub fn generate_moves<MoveGen: MoveGenerator + Component>(_event: On<GenerateMovesEvent>, mut commands: Commands, board: Single<&Board>,
+pub (crate) fn generate_moves<MoveGen: MoveGenerator + Component>(_event: On<GenerateMovesEvent>, mut commands: Commands, board: Single<&Board>,
     pieces: Query<(&PieceColor, &Position, &mut Moves, &MoveOnly<MoveGen>), With<Piece>>, piece_colors: Query<&PieceColor, With<Piece>>) {
 
     for (&color, &position, mut moves, MoveOnly(move_gen)) in pieces {
@@ -41,7 +41,7 @@ pub fn generate_moves<MoveGen: MoveGenerator + Component>(_event: On<GenerateMov
     }
 }
 
-pub fn generate_captures<MoveGen: MoveGenerator + Component>(_event: On<GenerateMovesEvent>, mut commands: Commands, board: Single<&Board>,
+pub (crate) fn generate_captures<MoveGen: MoveGenerator + Component>(_event: On<GenerateMovesEvent>, mut commands: Commands, board: Single<&Board>,
     pieces: Query<(&PieceColor, &Position, &mut Moves, &CaptureOnly<MoveGen>), With<Piece>>, piece_colors: Query<&PieceColor, With<Piece>>) {
 
     for (&color, &position, mut moves, CaptureOnly(move_gen)) in pieces {
@@ -49,7 +49,7 @@ pub fn generate_captures<MoveGen: MoveGenerator + Component>(_event: On<Generate
     }
 }
 
-pub fn generate_moves_and_captures<MoveGen: MoveGenerator + Component>(_event: On<GenerateMovesEvent>, mut commands: Commands, board: Single<&Board>,
+pub (crate) fn generate_moves_and_captures<MoveGen: MoveGenerator + Component>(_event: On<GenerateMovesEvent>, mut commands: Commands, board: Single<&Board>,
     pieces: Query<(&PieceColor, &Position, &mut Moves, &MoveGen), With<Piece>>, piece_colors: Query<&PieceColor, With<Piece>>) {
 
     for (&color, &position, mut moves, move_gen) in pieces {
@@ -57,7 +57,7 @@ pub fn generate_moves_and_captures<MoveGen: MoveGenerator + Component>(_event: O
     }
 }
 
-pub fn generate_marker_captures<MoveGen: MoveGenerator + Component, Marker: PieceMarker + Component + Copy>(_event: On<GenerateMovesEvent>, mut commands: Commands,
+pub (crate) fn generate_marker_captures<MoveGen: MoveGenerator + Component, Marker: PieceMarker + Component + Copy>(_event: On<GenerateMovesEvent>, mut commands: Commands,
     board: Single<&Board>, marker_board: Single<&MarkerBoard>,
     pieces: Query<(&PieceColor, &Position, &mut Moves, &CapturesMarkers<MoveGen, Marker>), With<Piece>>, piece_colors: Query<&PieceColor, With<Piece>>,
     piece_positions: Query<&Position, With<Piece>>, markers: Query<(&Marker, &Position)>) {
@@ -67,7 +67,7 @@ pub fn generate_marker_captures<MoveGen: MoveGenerator + Component, Marker: Piec
     }
 }
 
-pub fn generate_marker_captures_for_capture_only<MoveGen: MoveGenerator + Component, Marker: PieceMarker + Component + Copy>(_event: On<GenerateMovesEvent>, mut commands: Commands,
+pub (crate) fn generate_marker_captures_for_capture_only<MoveGen: MoveGenerator + Component, Marker: PieceMarker + Component + Copy>(_event: On<GenerateMovesEvent>, mut commands: Commands,
     board: Single<&Board>, marker_board: Single<&MarkerBoard>,
     pieces: Query<(&PieceColor, &Position, &mut Moves, &CaptureOnly<MoveGen>), (With<Piece>, With<CapturesMarkersAlways<Marker>>)>, piece_colors: Query<&PieceColor, With<Piece>>,
     piece_positions: Query<&Position, With<Piece>>, markers: Query<(&Marker, &Position)>) {
@@ -77,7 +77,7 @@ pub fn generate_marker_captures_for_capture_only<MoveGen: MoveGenerator + Compon
     }
 }
 
-pub fn generate_marker_captures_for_move_gen<MoveGen: MoveGenerator + Component, Marker: PieceMarker + Component + Copy>(_event: On<GenerateMovesEvent>, mut commands: Commands,
+pub (crate) fn generate_marker_captures_for_move_gen<MoveGen: MoveGenerator + Component, Marker: PieceMarker + Component + Copy>(_event: On<GenerateMovesEvent>, mut commands: Commands,
     board: Single<&Board>, marker_board: Single<&MarkerBoard>,
     pieces: Query<(&PieceColor, &Position, &mut Moves, &MoveGen), (With<Piece>, With<CapturesMarkersAlways<Marker>>)>, piece_colors: Query<&PieceColor, With<Piece>>,
     piece_positions: Query<&Position, With<Piece>>, markers: Query<(&Marker, &Position)>) {
@@ -97,7 +97,7 @@ macro_rules! register_marker {
 
 macro_rules! move_generator_plugin {
     ($plugin_name:ident,$move_generator:ty) => {
-        pub struct $plugin_name;
+        pub (crate) struct $plugin_name;
 
         impl Plugin for $plugin_name {
             fn build(&self, app: &mut App) {
@@ -111,5 +111,5 @@ macro_rules! move_generator_plugin {
     }
 }
 
-pub (crate) use register_marker;
-pub (crate) use move_generator_plugin;
+pub (crate) (crate) use register_marker;
+pub (crate) (crate) use move_generator_plugin;
